4b73b75f53d30d36d6623a15b9675508747d288a
budy6991 <alvmenfer@gmail.com>
2022-05-07 00:19:10 +0300

    Fixed bug, storing objects properly into array

--- script.js
+++ script.js
@@ -6,39 +6,40 @@ function Book(name, author, pages) {
   this.pages = pages 
 }
 
-const newBook = new Book ('The lurd of the rangs', 'JR Boind', '450')
-
-function addBookToLibrary() {
-    myLibrary.push(newBook)
+function addBookToLibrary(storeBook) {
+    myLibrary.push(storeBook)
 }
 
-/*----------When we press the add Button----------- */ 
+/*----------Pressing the add Button----------- */ 
 
 const btn = document.querySelector('.book')
 const formContainer = document.getElementById('form-container')
 
 btn.onclick = function () {
-
-  if (formContainer.style.display != "none") {
-    
-    formContainer.style.display = "none";
+  
+  if (formContainer.style.display == "none" || formContainer.style.display.length == 0) {
+    formContainer.style.display = "inline-block";
    
   } else {
-    formContainer.style.display = "inline-block";
+    formContainer.style.display = "none";
+
   }
 };
 
-/*----------When we press the addBook Button----------- */ 
+/*----------Pressing the addBook Button----------- */ 
 
 const btnAddBook = document.querySelector('.add-book')
 const inputName = document.getElementById('book-name')
 const inputAuthor = document.getElementById('author')
 const inputPages = document.getElementById('pages')
 
+
+
 btnAddBook.onclick = function (e){
   e.preventDefault();
-  console.log(inputName.value)
-  console.log(inputAuthor.value)
-  console.log(inputPages.value)
+  let storeBook = new Book (inputName.value , inputAuthor.value, inputPages.value)
+  addBookToLibrary(storeBook)
+  console.log(storeBook)
+  console.log(myLibrary)
 }
 

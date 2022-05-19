7e65546cc3b2f983a3b69cf99c93fc853182712a
budy6991 <alvmenfer@gmail.com>
2022-05-19 20:46:28 +0300

    Changed constructor by class

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let myLibrary = [];
 
-function Book(name, author, pages, read) {
-  this.name = name
-  this.author = author
-  this.pages = pages 
-  this.read = read
-  this.toggleRead = function (){
+class Book {
+  constructor (name, author, pages, read) {
+    this.name = name
+    this.author = author
+    this.pages = pages 
+    this.read = read
+  }
+  
+  toggleRead(){
 
     if (this.read == 'Yes') {
       this.read = 'No'
